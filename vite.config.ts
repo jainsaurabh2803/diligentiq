bb505a48b2d1f848c7617871792fdaff50678773
Saurabh Jain <saurabh.jain@rankz.io>
2025-11-28 23:24:48 +0530

    Use root base for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/diligentiq/',  // 👈 this is important for GitHub Pages project sites
+  base: '/',  // 👈 this is important for GitHub Pages project sites
   plugins: [react()],
 });
