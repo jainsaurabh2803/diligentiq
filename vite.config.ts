e4f84f874f9f67170429be07ccab5eedda9bea22
Saurabh Jain <saurabh.jain@rankz.io>
2025-12-01 18:09:57 +0530

    Enable clean routing without hash

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   base: '/',  // 👈 this is important for GitHub Pages project sites
+//   plugins: [react()],
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',  // 👈 this is important for GitHub Pages project sites
+  base: '/',   // ✅ required for clean routing + custom domain
   plugins: [react()],
 });
+
